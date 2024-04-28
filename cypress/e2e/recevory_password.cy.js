40c8ebc7ce6fe2d5d09e3289998d94e64bcf0810
Lucasfsa <lucas792azevedo@gmail.com>
2024-04-28 18:18:22 -0300

    melhorias no script cadastra-se e recuperar senha

--- cypress/e2e/recevory_password.cy.js
+++ cypress/e2e/recevory_password.cy.js
@@ -2,10 +2,11 @@ describe('Redirect Esqueci minha senha -> Cadastro', () => {
     it('Redirecionamento login', () => {
 
         cy.visit("https://enadex-v1.vercel.app/")
-        cy.contains('a.forgotPasswordLink', 'Esqueci minha senha').should('be.visible').click()
-      
-        cy.url().should('include', '/esqueci-minha-senha');
+        cy.get('.btn-acessar').click()
 
+        cy.get('.forgotPasswordLink').should('be.visible').click()
+        cy.url().should('include', '/esqueci-minha-senha');
+      
         cy.get('input').should('be.visible')
         cy.contains('button', 'Recuperar Senha').should('be.visible')
 
@@ -16,18 +17,25 @@ describe('Redirect Esqueci minha senha -> Cadastro', () => {
 });
 
 describe('Validação Cadastro', () => {
-    it('Validação de elentos', () => {
+    it('Validação de elementos', () => {
 
         cy.visit("https://enadex-v1.vercel.app/")
-        cy.contains('a.forgotPasswordLink', 'Esqueci minha senha').should('be.visible').click()
-      
+        cy.get('.btn-acessar').click()
+
+        cy.get('.forgotPasswordLink').should('be.visible').click()
         cy.url().should('include', '/esqueci-minha-senha');
+      
+        cy.get('#inputEmail').should('be.visible').type('jhondoe@gmail.com')       
 
-        cy.get('input').should('be.visible')
-        cy.contains('button', 'Recuperar Senha').should('be.visible')
+        cy.get('.recoverCode').should('be.visible').click()
+        cy.get('.modalRedefinir').should('be.visible')
 
-        
-        cy.contains('a.linkVoltar', 'Voltar').should('be.visible').click()
-        cy.url().should('include', '/login');
+        cy.get('#inputCodigo').should('be.visible').type('jhondoe@gmail.com')
+        cy.get('#inputSenha').should('be.visible').type('jhondoe@gmail.com')
+        cy.get('#inputConfirmacaoSenha').should('be.visible').type('jhondoe@gmail.com')
+
+        cy.get('.modalConfirmar > a').should('be.visible')
+        cy.get('.buttonRedefinir').should('be.visible')    
+        cy.get('.fechar').should('be.visible').click()
     });
 });
--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -1,22 +1,22 @@
 describe('Redirect Login -> Cadastro', () => {
     it('Redirecionamento login', () => {
 
-        cy.visit("https://enadex-v1.vercel.app/")
-        cy.contains('a.createAccount', 'Cadastrar-se').should('be.visible').click()
-      
+        cy.visit("https://enadex-v1.vercel.app")
+        cy.get('.btn-acessar').should('be.visible').click()
+        cy.get('.createAccount').should('be.visible').click()
         cy.url().should('include', '/register');
+        cy.contains('button', 'Voltar').should('be.visible')
 
-        cy.contains('button', 'Voltar').should('be.visible').click()
-        
-        cy.url().should('include', '/login');
+        cy.get('input[name="nameComplet"]').should('be.visible').type('John Doe Silvester');
+        cy.get('input[name="registrationNumber"]').should('be.visible').type('1231233');        
+        cy.get('input[name="password"]').should('be.visible').type('123456');        
+        cy.get('input[name="confirmPassword"]').should('be.visible').type('123456');        
+        cy.get('input[name="email"]').should('be.visible').type('jhondoe@gmail.com');        
+        cy.get('input[name="course"]').should('be.visible').type('Sistema de informação');        
+        cy.get('input[name="currentSemester"]').should('be.visible').type('9');        
+        cy.get('#selectUnit').should('be.visible').select(1); 
+      
     });
 });
 
-/*describe('Validação de cadastro', () => {
-    it('Validação de elementos', () => {
-
-        cy.visit("https://enadex-v1.vercel.app/register")
-        cy.get('body')
-    });
-});*/
 
