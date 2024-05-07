cddc7be386b43bf6f072718c7f183b4b573dbcf4
Lucasfsa <lucas792azevedo@gmail.com>
2024-05-07 16:45:35 -0300

    adição de script de cadastro de professor

--- cypress/e2e/signup_teatcher.cy.js
+++ cypress/e2e/signup_teatcher.cy.js
@@ -1,22 +1,23 @@
-describe('Redirect Login -> Cadastro', () => {
-    it('Redirecionamento login', () => {
+describe('Signup teatcher', () => {
+    it('Cadastro professor', () => {
 
         cy.visit("https://enadex-v1.vercel.app")
         cy.get('.btn-acessar').should('be.visible').click()
-        cy.get('.createAccount').should('be.visible').click()
-        cy.url().should('include', '/register');
-        cy.contains('button', 'Voltar').should('be.visible')
 
+        cy.get('[type="email"]').should('be.visible').type('qa@gmail.com');
+        cy.get('.passwordInput > input').should('be.visible').type('qa@123');
+        cy.get('#criarButton').click()
+
+        cy.get('[href="/register-admin-teacher"]').should('be.visible').click();
+
+        
         cy.get('input[name="nameComplet"]').should('be.visible').type('John Doe Silvester');
-        cy.get('input[name="registrationNumber"]').should('be.visible').type('1231233');        
         cy.get('input[name="password"]').should('be.visible').type('123456');        
         cy.get('input[name="confirmPassword"]').should('be.visible').type('123456');        
         cy.get('input[name="email"]').should('be.visible').type('jhondoe@gmail.com');        
         cy.get('input[name="course"]').should('be.visible').type('Sistema de informação');        
-        cy.get('input[name="currentSemester"]').should('be.visible').type('9');        
         cy.get('#selectUnit').should('be.visible').select(1); 
+        cy.contains('button', 'Cadastrar').should('be.visible')
 
     });
 });
-
-
