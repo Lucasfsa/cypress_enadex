4603adae961426c032ef9cb9d0a2593f92fe8b74
Lucasfsa <lucas792azevedo@gmail.com>
2024-05-19 22:06:22 -0300

    atualizações no script login e signup

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,40 +1,42 @@
 describe('Elementos login', () => {
     it('Validação de elementos', () => {
 
-        cy.visit("https://enadex-v1.vercel.app/login")
+        cy.visit("https://enadex-v1.vercel.app")
+        cy.get('#navLinksEntrar').should('be.visible').click()
 
-        cy.get('.logo').should('be.visible')
+        cy.get('#email').should('be.visible');
+        cy.get('#password').should('be.visible');
 
-        cy.get('[type="email"]').should('be.visible').type('qa@gmail.com');
-        cy.get('.passwordInput > input').should('be.visible').type('qa@123');
+        cy.get('#exampleCheck1').should('be.visible');
+        cy.get('#exampleCheck1 + label').should('have.text', 'MANTER CONECTADO');
+     
+        cy.get('.links-modal-forgot').should('have.attr', 'href', '/');
+        cy.get('.links-modal-forgot').should('have.text', 'ESQUECEU A SENHA?');
 
-        cy.get('[type="email"]').should('be.enabled');
-        cy.get('.passwordInput > input').should('be.enabled');
+        cy.get('.links-modal-login[href="/termos-uso"]').should('have.text', 'Termos de Uso');
+        cy.get('.links-modal-login[href="/politica-privacidade"]').should('have.text', 'Política de Privacidade');
 
-        cy.get('.buttonLogin').should('be.visible')
+        cy.get('.modal-body button[type="submit"]').should('have.text', 'ENTRAR');
 
-        cy.get('.forgotPasswordLink').should('be.visible').click()
-        cy.url().should('include', '/esqueci-minha-senha');
-        cy.visit("https://enadex-v1.vercel.app/login")
-
-
-        cy.get('.createAccount').should('be.visible').click()
-        cy.url().should('include', '/register');
-        cy.visit("https://enadex-v1.vercel.app/login")
+        
+        cy.get('.links-modal-login[href="/"]').should('have.text', 'Crie sua conta aqui');
 
     });
 });
 
 describe('Tela login', () => {
-    it('Validação de redirecionamento', () => {
+    it('Validação de redirecionamento', () => {     
 
-        cy.visit("https://enadex-v1.vercel.app/login")
+        cy.visit("https://enadex-v1.vercel.app")
 
-        cy.get('[type="email"]').should('be.visible').type('qa@gmail.com');
-        cy.get('.passwordInput > input').should('be.visible').type('qa@123');
+        cy.get('#navLinksEntrar').should('be.visible').click()
 
-        cy.get('.buttonLogin').click()
-        cy.url().should('include', '/');
+        cy.get('#email').should('be.visible').type('qa@gmail.com');
+        cy.get('#password').should('be.visible').type('qa@123');
+
+        cy.get('#exampleCheck1').should('be.visible').click()
+        cy.contains('button', 'ENTRAR').should('be.visible').click()
+        cy.url().should('include', '/home');
 
     });
 });
--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -1,22 +1,74 @@
-describe('Redirect Login -> Cadastro', () => {
-    it('Redirecionamento login', () => {
-
+describe('Validação de elementos', () => {
+    it('Deve exibir todos os campos e validações', () => {
+            
         cy.visit("https://enadex-v1.vercel.app")
-        cy.get('.btn-acessar').should('be.visible').click()
-        cy.get('.createAccount').should('be.visible').click()
-        cy.url().should('include', '/register');
-        cy.contains('button', 'Voltar').should('be.visible')
-
-        cy.get('input[name="nameComplet"]').should('be.visible').type('John Doe Silvester');
-        cy.get('input[name="registrationNumber"]').should('be.visible').type('1231233');        
-        cy.get('input[name="password"]').should('be.visible').type('123456');        
-        cy.get('input[name="confirmPassword"]').should('be.visible').type('123456');        
-        cy.get('input[name="email"]').should('be.visible').type('jhondoe@gmail.com');        
-        cy.get('input[name="course"]').should('be.visible').type('Sistema de informação');        
-        cy.get('input[name="currentSemester"]').should('be.visible').type('9');        
-        cy.get('#selectUnit').should('be.visible').select(1); 
-
-    });
+        cy.get('#navLinksEntrar').should('be.visible').click()
+        cy.get('.links-modal-login[href="/"]').should('have.text', 'Crie sua conta aqui').click();
+    
+        cy.get('label[for="name"]').should('have.text', 'Nome');
+        cy.get('#name').should('be.visible');
+    
+        cy.get('label[for="email"]').should('have.text', 'Email *');
+        cy.get('#email').should('be.visible');
+        cy.get('#email').should('have.attr', 'required');
+    
+        cy.get('label[for="curso"]').should('have.text', 'Curso');
+        cy.get('#cursoSelect').should('be.visible');
+        cy.get('#cursoSelect option').should('have.length', 2);
+    
+        cy.get('label[for="unidade"]').should('have.text', 'Unidade');
+        cy.get('#unidade').should('be.visible');
+        cy.get('#unidade option').should('have.length', 6);
+    
+        cy.get('label[for="matricula"]').should('have.text', 'Matrícula');
+        cy.get('#matricula').should('be.visible');
+    
+        cy.get('label[for="semestre"]').should('have.text', 'Semestre');
+        cy.get('#semestre').should('be.visible');
+        cy.get('#semestre option').should('have.length', 11);
+    
+        cy.get('label[for="password"]').should('have.text', 'Senha');
+        cy.get('#password').should('be.visible');
+    
+        cy.get('label[for="confirmPassword"]').should('have.text', 'Confirmar Senha');
+        cy.get('#confirmPassword').should('be.visible');
+    
+        cy.get('.links-modal-login[href="/termos-uso"]').should('have.text', 'Termos de Uso');
+        cy.get('.links-modal-login[href="/politica-privacidade"]').should('have.text', 'Política de Privacidade');
+    
+        cy.get('.modal-body button[type="submit"]').should('have.text', 'CRIAR CONTA');
+    
+        cy.get('.links-modal-login[href="/"]').should('have.text', 'Faça login aqui');
+      });
+    
 });
+
+describe('Modal de Cadastro', () => {
+   
+    it('Deve preencher todos os campos do formulário', () => {
+
+      cy.visit("https://enadex-v1.vercel.app")
+      cy.get('#navLinksEntrar').should('be.visible').click()
+      cy.get('.links-modal-login[href="/"]').should('have.text', 'Crie sua conta aqui').click();
+
+      cy.get('#name').type('John Doe');
+  
+      cy.get('#email').type('johndoe@example.com');
+  
+      cy.get('#cursoSelect').select('6636d04c2a9c00506b72601b');
+  
+      cy.get('#unidade').select('SALVADOR');
+  
+      cy.get('#matricula').type('123456');
+  
+      cy.get('#semestre').select('5');
+  
+      cy.get('#password').type('mypassword');
+  
+      cy.get('#confirmPassword').type('mypassword');
+  
+    //   cy.get('.modal-body button[type="submit"]').click();
+    });
+  });
 
 
